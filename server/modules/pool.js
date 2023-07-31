f57f78ea235d331cf6757244bd363d258d4f3ac5
Ben Lynch <bdlynch7@gmail.com>
2023-07-31 18:34:05 -0500

    update pool.js for deploy

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,9 +1,23 @@
 // Setup Pool
 const pg = require ('pg')
-let pool = new pg.Pool({
-  host: 'localhost',
-  port: 5432,
-  database: 'weekend-to-do-app'
-})
+let pool;
+
+if (process.env.DATABASE_URL) {
+  pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+
+    }
+  })
+}
+else {
+  pool = new pg.Pool({
+    host: 'localhost',
+    port: 5432,
+    database: 'todoapp_qawb'
+  })
+}
+
 
 module.exports = pool
